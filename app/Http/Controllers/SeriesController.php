6921ace780926a588bb1953a327d14ded3901d45
cleberpereiradev <cleberpereiraeng@gmail.com>
2023-12-07 18:12:27 -0300

    CRUD finish

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -11,8 +11,11 @@ class SeriesController extends Controller
     public function index(Request $request)
     {
         $series = Serie::query()->orderBy('nome')->get();
+        $mensagemSucesso = $request->session()->get('mensagem.sucesso');
+
         return view('series.index')
-            -> with('series', $series);
+            -> with('series', $series)
+            -> with('mensagemSucesso', $mensagemSucesso);
     }
 
 
@@ -23,13 +26,32 @@ class SeriesController extends Controller
 
     public function store(Request $request)
     {
-        $nomeSerie = $request->input('nome');
+        $serie = Serie::create($request->all());
 
-        $serie = new Serie();
-        $serie->nome = $nomeSerie;
-        $serie->save();
-
-
-        return redirect("/series");
+        return to_route('series.index')
+            ->with('mensagem.sucesso', "A série '{$serie->nome}' foi cadastrada com sucesso!");
     }
+
+    public function destroy(Serie $series)
+    {
+        $series->delete();
+
+        return to_route('series.index')
+            ->with('mensagem.sucesso', "A série '{$series->nome}' foi removida com sucesso!");
+    }
+
+    public function edit(Serie $series)
+    {
+        return view('series.edit')->with('serie', $series);
+    }
+
+    public function update(Serie $series, Request $request)
+    {
+        $series->nome = $request->nome;
+        $series->save();
+
+        return to_route('series.index')
+            ->with('mensagem.sucesso', "A série '{$series->nome}' foi alterada com sucesso!");
+    }
+
 }
--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -1,8 +1,28 @@
 <x-layout title="Séries">
-    <a href="/series/criar" class="btn btn-warning my-3">Adicionar nova série</a>
+    <a href="{{ route('series.create') }}" class="btn btn-warning my-3">Adicionar nova série</a>
+    @isset($mensagemSucesso)
+        <div class="alert alert-success">
+            {{ $mensagemSucesso }}
+        </div>
+    @endisset
     <ul class="list-group">
         @foreach ($series as $serie)
-            <li class="list-group-item">{{ $serie->nome }}</li>
+            <li class="list-group-item d-flex justify-content-between align-items-center col-8">
+                {{ $serie->nome }}
+                <span class="d-flex gap-2">
+                    <a href="{{ route('series.edit', $serie->id) }}" class="btn btn-warning btn-sm">
+                        E
+                    </a>
+
+                    <form action="{{ route('series.destroy', $serie->id) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn btn-danger btn-sm">
+                            X
+                        </button>
+                    </form>
+                </span>
+            </li>
         @endforeach
     </ul>
 </x-layout>
